8208c3783858af87c318eccaebd403d51626108c
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-20 19:55:19 +0600

    used short form of 'exact={true}' (recommended)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route path="/" exact={true}>
+        <Route path="/" exact>
           <AllMeetupsPage />
         </Route>
         <Route path="/new-meetup">
