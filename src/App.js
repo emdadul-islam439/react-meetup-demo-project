d68857b535b2e60d476d3a7647cdc4eef50b9b8c
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-20 19:52:58 +0600

    experiment: in react-router-dom: 5.x.x-> for Switch, if not use 'exact' first matching sub-string path will be visited. in this case-> for '/new-meetup' path, '/' will be visited. Because '/' is a substring of '/new-meetup' and it appears first

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route path="/" exact>
+        <Route path="/">
           <AllMeetupsPage />
         </Route>
         <Route path="/new-meetup">
