b61bb71d6ffb47ebfb79416a77d45582129ffc9e
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-21 19:49:24 +0600

    ch-02, vid-24 started-> added NewMeetupForm and related css file to create form

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,11 @@
+import classes from "./NewMeetupForm.module.css";
+
+function NewMeetupForm() {
+    return <div className={classes.form}>
+        <div className={classes.actions}>
+            
+        </div>
+    </div>;
+}
+
+export default NewMeetupForm
