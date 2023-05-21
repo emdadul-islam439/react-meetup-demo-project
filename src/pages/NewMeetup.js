e06c25f906b100e79ab321afa65ac25c016911f8
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-21 21:45:54 +0600

    finished ch-02, vid-26-> successfully passed data from NewMeetupForm(child-component) to NewMeetupPage(Parent-component)

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,16 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  return <section>
-    <h1>Add New Meetup</h1>
-    <NewMeetupForm />
-  </section>;
+  function onAddMeetupHandler(meetupData) {
+    console.log("onAddMeetupHandler");
+    console.log(meetupData);
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={onAddMeetupHandler}/>
+    </section>
+  );
 }
 
 export default NewMeetupPage;
