aea79240b22a20ae9162d049175c89bf64681358
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-21 19:31:48 +0600

    learned passing multiple-child coponent by-> 'created Card component for wrapping items into a card'

---
+++ src/components/meetups/MeetupList.js
@@ -0,0 +1,24 @@
+import Card from "../ui/Card";
+import MeetupItem from "./MeetupItem";
+import classes from "./MeetupList.module.css";
+
+function MeetupList(props) {
+  return (
+    <ul className={classes.list}>
+      {props.meetups.map((meetup) => (
+        <Card>
+          <MeetupItem
+            key={meetup.id}
+            id={meetup.id}
+            image={meetup.image}
+            title={meetup.title}
+            address={meetup.address}
+            description={meetup.description}
+          />
+        </Card>
+      ))}
+    </ul>
+  );
+}
+
+export default MeetupList;
