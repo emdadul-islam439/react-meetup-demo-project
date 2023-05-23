c251a61b8670a3adb7ad058aed1adbfd0764a7c2
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-23 19:41:51 +0600

    finished ch-02, vid-21-> succesfully sent data into firebase realtime DB

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -4,6 +4,17 @@ function NewMeetupPage() {
   function onAddMeetupHandler(meetupData) {
     console.log("onAddMeetupHandler");
     console.log(meetupData);
+    // db-link: https://react-getting-started-34414-default-rtdb.firebaseio.com/
+    fetch(
+      'https://react-getting-started-34414-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }
+    )
   }
   return (
     <section>
