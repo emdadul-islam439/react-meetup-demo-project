547cb96cdb95dd5f50e32d537e9d4d0bd8836cbf
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-23 22:23:56 +0600

    connected meetupItem with global context and changed the 'add to favourite' btn text accordingly

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,6 +1,26 @@
+import { useContext } from "react";
+
 import classes from "./MeetupItem.module.css";
+import FavouriteContext from "../../store/favourites-context";
 
 function MeetupItem(props) {
+  const favouriteContext = useContext(FavouriteContext);
+
+  const isFavourite = favouriteContext.itemIsFavourite(props.id);
+
+  function toggleFavouriteStatusHandler() {
+    if (isFavourite) {
+      favouriteContext.removeFavoute(props.id);
+    } else {
+      favouriteContext.addFavourite({
+        id: props.id,
+        title: props.title,
+        image: props.image,
+        address: props.address,
+        description: props.description,
+      });
+    }
+  }
   return (
     <li className={classes.item}>
       <div className={classes.image}>
@@ -12,7 +32,9 @@ function MeetupItem(props) {
         <p>{props.description}</p>
       </div>
       <div className={classes.actions}>
-        <button>Add To Favourite</button>
+        <button onClick={toggleFavouriteStatusHandler}>
+          {isFavourite ? "Remove From Favourite" : "Add To Favourite"}
+        </button>
       </div>
     </li>
   );
