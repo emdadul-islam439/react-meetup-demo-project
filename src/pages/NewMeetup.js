4aae1bc80d0e83b24a12db6fb4d9a61cde749489
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-23 19:50:07 +0600

    finished ch-02, vid-28-> successfully go to HOME PAGE when new-meetup successfully submits

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,25 +1,30 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
   function onAddMeetupHandler(meetupData) {
     console.log("onAddMeetupHandler");
     console.log(meetupData);
     // db-link: https://react-getting-started-34414-default-rtdb.firebaseio.com/
     fetch(
-      'https://react-getting-started-34414-default-rtdb.firebaseio.com/meetups.json',
+      "https://react-getting-started-34414-default-rtdb.firebaseio.com/meetups.json",
       {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify(meetupData),
         headers: {
-          'Content-Type': 'application/json',
-        }
+          "Content-Type": "application/json",
+        },
       }
-    )
+    ).then(() => {
+      history.replace("/");
+    });
   }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm onAddMeetup={onAddMeetupHandler}/>
+      <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
     </section>
   );
 }
