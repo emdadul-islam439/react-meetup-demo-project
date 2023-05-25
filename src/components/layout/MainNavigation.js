63079f5f06455f5ca091194a629f3d40bf4afa40
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-25 10:05:12 +0600

    finished ch-02, vid-34-> created badge to show favourite-item count in the main-navigation-bar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+
 import classes from "./MainNavigation.module.css";
+import FavouriteContext from "../../store/favourites-context";
 
 function MainNavigation() {
+  const favouriteContext = useContext(FavouriteContext);
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -14,7 +18,10 @@ function MainNavigation() {
             <Link to="/new-meetup">New Meetup</Link>
           </li>
           <li>
-            <Link to="/favourites">Favourites</Link>
+            <Link to="/favourites">
+              Favourites
+              <span className={classes.badge}>{favouriteContext.totalFavourites}</span>
+            </Link>
           </li>
         </ul>
       </nav>
